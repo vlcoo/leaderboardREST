5cd7e4a312d35ebc14d216c4f2cf1946fecb1b26
vic <victorstancioiu@gmail.com>
2023-12-12 18:07:24 +0100

    añadir calculo de score y post con sort

--- app.py
+++ app.py
@@ -5,26 +5,44 @@
 from flask import Flask, request
 import pickle
 import os.path
+import enum
+
+
+class SortType(enum.Enum):
+    SCORE = "score"
+    FLOORS = "floors"
+    KILLS = "kills"
+    BOSS_KILLS = "boss_kills"
+    TIME = "time"
+
 
 class LeaderboardEntry:
-    def __init__(self, pname: str, floors: int, kills: int, bossKills: int, time: int):
+    def __init__(self, pname: str, floors: int, kills: int, boss_kills: int, time: int):
         self.pname = pname
         self.floors = floors
         self.kills = kills
-        self.bossKills = bossKills
+        self.boss_kills = boss_kills
         self.time = time
+        self.score = self.get_score()
 
-    def serialize(self):
+    def __lt__(self, other):
+        return self.get_score() < other.get_score()
+
+    def serialize(self) -> dict:
         return {
             "pname": self.pname,
             "floors": self.floors,
             "kills": self.kills,
-            "bossKills": self.bossKills,
-            "time": self.time
+            "boss_kills": self.boss_kills,
+            "time": self.time,
+            "score": self.get_score(),
         }
 
+    def get_score(self) -> int:
+        return self.kills * 200 + self.boss_kills * 2000 + self.floors * 750 + self.time * -1
+
     def __str__(self):
-        return self.pname
+        return f"{self.pname} ({self.score} pts.)"
 
 
 app = Flask(__name__)
@@ -37,7 +55,7 @@ PAGE_SIZE = 8
 @app.route('/', methods=['GET'])
 def hello_world() -> str:
     return f"""Hola! rest api roguelike. uso:
-        - GET /leadeboard (solo los {PAGE_SIZE} primeros)
+        - GET /leaderboard (solo los {PAGE_SIZE} primeros)
         - GET /leaderboard?page=x (paginado {PAGE_SIZE} jugadores a la vez, empezando por 0)
         - POST /newEntry (dados pname, floors, kills, bossKills y time)
     """
@@ -45,29 +63,47 @@ def hello_world() -> str:
 
 @app.route('/leaderboard', methods=['GET'])
 def get_leaderboard() -> dict:
+    if "page" not in request.args:
+        return {
+            "player_count": len(leaderboard_db),
+            "players_per_page": PAGE_SIZE,
+        }
+
     page = request.args.get("page", 0, type=int)
+    sort_type = request.args.get("sort", SortType.SCORE.value, type=str)
     print(f"splicing at page {page}!!")
 
-    if page > (len(leaderboard_db) // PAGE_SIZE):
+    if page > (len(leaderboard_db) // PAGE_SIZE) or not leaderboard_db:
         return {"leaderboard": []}
-    return {"leaderboard": [entry.serialize() for entry in leaderboard_db[page * PAGE_SIZE: (page + 1) * PAGE_SIZE]]}
+
+    return {"leaderboard": [entry.serialize() for entry in get_sorted_leaderboard_by(sort_type)[page * PAGE_SIZE: (page + 1) * PAGE_SIZE]]}
+
 
 # endregion
 
 
 # region Métodos POST
 @app.route('/newEntry', methods=['POST'])
-def new_entry():
+def new_entry() -> str:
     entry = LeaderboardEntry(pname=request.json['pname'],
                              floors=request.json['floors'],
                              kills=request.json['kills'],
-                             bossKills=request.json['bossKills'],
+                             boss_kills=request.json['boss_kills'],
                              time=request.json['time'])
     add_entry_and_save(entry)
-    return {'pname':request.json['pname']}
+    return entry.pname
+
+
 # endregion
 
 
+def get_sorted_leaderboard_by(sort_type: str) -> list[LeaderboardEntry]:
+    if sort_type not in [sort_type.value for sort_type in SortType]:
+        sort_type = SortType.SCORE.value
+    return sorted(leaderboard_db, key=lambda entry: getattr(entry, sort_type), reverse=True)
+
+
+# adds an entry to the leaderboard list and saves it to file
 def add_entry_and_save(entry: LeaderboardEntry) -> None:
     leaderboard_db.append(entry)
     with open("leaderboard.pkl", "wb") as f:
